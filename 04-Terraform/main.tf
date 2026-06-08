a75461b93c0b18fb5381bc779a8fd7a1976cc2d6
Maximus Ikengwa <chinazomaxi@gmail.com>
2026-06-07 18:56:49 -0600

    Add VPC resource: cyber-lab-v2-vpc created by Terraform-runner in ca-central-1 - 10.0.0.0/16

--- 04-Terraform/main.tf
+++ 04-Terraform/main.tf
@@ -11,3 +11,13 @@ terraform {
 provider "aws" {
   region = "ca-central-1"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "cyber-lab-v2-vpc"
+  }
+}
